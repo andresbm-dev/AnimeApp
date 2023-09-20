08e7889318856248c5e0062cf85016f0ea644eef
Andres.bonilla <andres.bonilla@xpertgroup.co>
2023-09-19 23:40:41 -0500

    feat: create fragment to screen serach

--- app/src/main/java/com/example/animeapp/presentation/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/animeapp/presentation/fragments/SearchFragment.kt
@@ -5,18 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.animeapp.R
+import com.example.animeapp.databinding.FragmentDetailAnimeBinding
+import com.example.animeapp.databinding.FragmentMainAnimeBinding
+import com.example.animeapp.databinding.FragmentSearchBinding
+import com.example.animeapp.presentation.viewmodel.MainAnimeViewModel
 
 
 class SearchFragment : Fragment() {
-
+    private lateinit var binding: FragmentSearchBinding
+    private lateinit var viewModel: MainAnimeViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+    ): View {
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.backButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 
 
