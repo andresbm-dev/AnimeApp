c643fd5d6a38267f4f81dde01acd029a5086cacc
Andres.bonilla <andres.bonilla@xpertgroup.co>
2023-09-20 20:31:34 -0500

    feat: implement feature search anime

--- app/src/main/java/com/example/animeapp/presentation/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/animeapp/presentation/fragments/SearchFragment.kt
@@ -1,22 +1,32 @@
 package com.example.animeapp.presentation.fragments
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import com.example.animeapp.R
-import com.example.animeapp.databinding.FragmentDetailAnimeBinding
-import com.example.animeapp.databinding.FragmentMainAnimeBinding
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.animeapp.databinding.FragmentSearchBinding
+import com.example.animeapp.domain.models.AnimeTopScoreModel
+import com.example.animeapp.presentation.adapters.CarrouselAnimeTopAdapter
 import com.example.animeapp.presentation.viewmodel.MainAnimeViewModel
 
 
 class SearchFragment : Fragment() {
     private lateinit var binding: FragmentSearchBinding
     private lateinit var viewModel: MainAnimeViewModel
+    private var animeTopScore = mutableListOf<AnimeTopScoreModel>()
 
+    private val animeTopAdapter by lazy{
+        CarrouselAnimeTopAdapter{anime->
+
+        }
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -28,14 +38,59 @@ class SearchFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(requireActivity())[MainAnimeViewModel::class.java]
 
+        binding.rvAnimeTopScore.apply {
+            layoutManager = LinearLayoutManager(requireActivity(), LinearLayoutManager.VERTICAL, false)
+            adapter = animeTopAdapter
+        }
         initListeners()
+        initObservers()
+
+    }
+
+    private fun initObservers() {
+        viewModel.animeTopListLiveData.observe(viewLifecycleOwner){
+            animeTopScore = it as MutableList<AnimeTopScoreModel>
+        }
     }
 
     private fun initListeners() {
         binding.backButton.setOnClickListener {
             findNavController().popBackStack()
         }
+        binding.containerView.setOnClickListener {
+            hideKeyboard()
+        }
+
+
+        binding.etSearch.addTextChangedListener { filter ->
+            if (animeTopScore.isNotEmpty()) {
+                val countriesFilters: List<AnimeTopScoreModel> = animeTopScore.filter { anime ->
+                    anime.animeTitle?.lowercase()
+                        ?.contains(filter.toString().lowercase())!!
+                }
+                if (countriesFilters.isNotEmpty() && !filter.isNullOrEmpty()){
+                    binding.rvAnimeTopScore.visibility = View.VISIBLE
+                    animeTopAdapter.submitList(countriesFilters.toMutableList())
+                    binding.tvSearchResult.visibility = View.GONE
+                }else if (filter.isNullOrEmpty()){
+                    binding.rvAnimeTopScore.visibility = View.GONE
+                    binding.tvSearchResult.text = "Ingresa Busqueda"
+                    binding.tvSearchResult.visibility = View.VISIBLE
+                }else{
+                    binding.tvSearchResult.text = "No se encuentran resultados"
+                    binding.tvSearchResult.visibility = View.VISIBLE
+                    binding.rvAnimeTopScore.visibility = View.GONE
+
+                }
+            }
+        }
+    }
+
+    private fun hideKeyboard() {
+        val imm = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(requireView().windowToken, 0)
     }
 
 
